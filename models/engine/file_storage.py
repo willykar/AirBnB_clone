6db536ca7a48d0d5d9d31861db9ea27566f287df
willykar <wilsonnjenga799@gmail.com>
2024-02-10 05:21:36 -0800

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,39 +1,25 @@
 #!/usr/bin/env python3
-"""Defines file_storage.py model"""
-
 import json
 from models.base_model import BaseModel
 
 class FileStorage:
-    """Represents a FileStorage class"""
-
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """returns the dictionary __objects"""
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id"""
-        class_name = obj.__class__.__name__
-        FileStorage.__objects["{}.{}".format(class_name, obj.id)] = obj
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
 
     def save(self):
-        """serializes __objects to the JSON file (path: __file_path)"""
-        file_obj = FileStorage.__objects
-        new_dict = {obj: file_obj[obj].to_dict() for obj in file_obj.keys()}
-        with open(FileStorage.__file_path, "w") as f:
-            json.dump(new_dict, f)
+        with open(self.__file_path, "w") as f:
+            json.dump(self.__objects, f)
 
     def reload(self):
-        """deserializes the JSON file to __objects (only if the JSON
-        file (__file_path) exists ; otherwise, do nothing. If the 
-        file doesn’t exist, no exception should be raised)
-        """
         try:
-            with open(FileStorage.__file_path, "r") as f:
-                new_dict = json.load(f)
+            with open(self.__file_path, "r") as f:
+                self.__objects = json.load(f)
         except FileNotFoundError:
-            return
-
+            pass  # No exception if file doesn't exist
