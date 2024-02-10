9584d3e12bc13868805dd35affc42f42691fecc1
willykar <wilsonnjenga799@gmail.com>
2024-02-10 05:59:11 -0800

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,45 +1,38 @@
-#!/usr/bin/python3
-"""Defines the FileStorage module"""
+#!/usr/bin/env python3
+"""Defines file_storage.py model"""
+
 import json
 from models.base_model import BaseModel
 
-
 class FileStorage:
-    """Represent a class FileStorage
+    """Represents a FileStorage class"""
 
-    Attributes:
-        __file_path (str): The name of the file to save objects
-        __objects (dict): A dictionary of instantiated objects
-    """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """Return the dictionary __objects"""
+        """returns the dictionary __objects"""
         return FileStorage.__objects
 
     def new(self, obj):
-        """Set in __objects obj with key <obj_class_name>.id"""
-        ocname = obj.__class__.__name__
-        FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
+        """sets in __objects the obj with key <obj class name>.id"""
+        class_name = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(class_name, obj.id)] = obj
 
     def save(self):
-        """Serialize __objects to the JSON file"""
+        """serializes __objects to the JSON file (path: __file_path)"""
         file_obj = FileStorage.__objects
-        obj_dict = {obj: file_obj[obj].to_dict() for obj in file_obj.keys()}
+        new_dict = {obj: file_obj[obj].to_dict() for obj in file_obj.keys()}
         with open(FileStorage.__file_path, "w") as f:
-            json.dump(obj_dict, f)
+            json.dump(new_dict, f)
 
     def reload(self):
-        """deserializes the JSON file to __objects (only if the JSO
-        N file (__file_path) exists ; otherwise, do nothing. If
-        the file doesn’t exist, no exception should be raised)"""
+        """deserializes the JSON file to __objects (only if the JSON
+        file (__file_path) exists ; otherwise, do nothing. If the 
+        file doesn’t exist, no exception should be raised)
+        """
         try:
-            with open(FileStorage.__file_path) as f:
-                obj_dict = json.load(f)
-                for i in obj_dict.values():
-                    cls_name = i["__class__"]
-                    del i["__class__"]
-                    self.new(eval(cls_name)(**i))
+            with open(FileStorage.__file_path, "r") as f:
+                new_dict = json.load(f)
         except FileNotFoundError:
             return
