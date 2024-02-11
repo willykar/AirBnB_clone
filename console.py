960a4c077c6ce1a1d03b146bb220e33e08481f89
willykar <wilsonnjenga799@gmail.com>
2024-02-11 11:32:55 -0800

    update

--- console.py
+++ console.py
@@ -91,17 +91,19 @@ class HBNBCommand(cmd.Cmd):
         instances,optionally filtered by class"""
         args = arg.split()
         all_obj = models.storage.all()
+        if len(args) == 0:
+            print("** class name missing **")
+            return
 
-        if len(args) > 0 and args[0] not in HBNBCommand.__valid_classes:
+        class_name = args[0]
+
+        if class_name not in HBNBCommand.__valid_classes:
             print("** class doesn't exist **")
-        else:
-            valid_objs = []
-            for obj in all_obj.values():
-                if len(args) > 0 and args[0] == obj.__class__.__name__:
-                    valid_objs.append(obj.__str__())
-                elif len(args) == 0:
-                    valid_objs.append(obj.__str__())
-            print(valid_objs)
+            return
+
+        class_instances = [str(obj) for obj in all_obj.values() if obj.__class__.__name__ == class_name]
+        print(class_instances)
+
 
     def do_update(self, arg):
         """Updates an instance based on the class name and id by
