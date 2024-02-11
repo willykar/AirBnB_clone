def3ded74d7ef9fc28fe58e6c5ad65b1db7fdc88
willykar <wilsonnjenga799@gmail.com>
2024-02-11 09:25:28 -0800

    update

--- console.py
+++ console.py
@@ -5,6 +5,11 @@
 import cmd
 import shlex
 import models
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 from models.base_model import BaseModel
 from models.user import User
 from models import storage
