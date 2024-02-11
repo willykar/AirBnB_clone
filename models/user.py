16ab3c8a5def192016cfa2b2902dda05efec2cb4
willykar <wilsonnjenga799@gmail.com>
2024-02-11 08:35:47 -0800

    update

--- models/user.py
+++ models/user.py
@@ -5,7 +5,7 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """Creates a new user that inherits from BaseModel"""
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    email: str  = ""
+    password: str = ""
+    first_name: str = ""
+    last_name: str = ""
